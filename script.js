30bd102d8b2d4c0ef97a642f1089829282c655aa
Victor8522 <victorpesty0508@gmail.com>
2026-03-13 21:05:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function getMeteoSync(link = "https://api.open-meteo.com/v1/forecast?latitude=47.5943&longitude=1.3291&daily=weather_code,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Europe%2FBerlin&forecast_days=1") {
+function getDataSync(link = "https://api.open-meteo.com/v1/forecast?latitude=47.5943&longitude=1.3291&daily=weather_code,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Europe%2FBerlin&forecast_days=1") {
     try {
         const xhr = new XMLHttpRequest();
         xhr.open("GET", link, false);
@@ -13,7 +13,7 @@ function getMeteoSync(link = "https://api.open-meteo.com/v1/forecast?latitude=47
     }
 }
 
-var data = getMeteoSync();
+// var data = getDataSync();
 
 // const date = document.getElementById('date');
 // date.innerHTML = data.daily.time[0];
@@ -41,7 +41,7 @@ var data = getMeteoSync();
 
 // const refreshbtn = document.getElementById('refresh-btn');
 
-const container =  document.querySelector('.container');
+const container = document.querySelector('.container');
 
 function newCard(id) {
     let card = document.createElement('div');
@@ -123,7 +123,6 @@ function detailCard(parm, id, weather_details) {
     value_span.appendChild(value);
 }
 
-
 function updateCard(ville, data) {
     let id = ville.getId();
 
@@ -152,24 +151,52 @@ function updateCard(ville, data) {
 
 
 class Ville {
-  constructor(id, nom) {
-    this.id = id;
-    this.nom = nom;
-  }
+    constructor(id, nom, latitude, longitude) {
+        this.id = id;
+        this.nom = nom;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
-  getId() {
-    return this.id;
-  }
+    getId() {
+        return this.id;
+    }
 
-  getNom() {
-    return this.nom;
-  }
+    getNom() {
+        return this.nom;
+    }
 
+    getLatitude() {
+        return this.latitude;
+    }
+
+    getLongitude() {
+        return this.longitude;
+    }
 }
 
-const villes = [
-    new Ville(1, "Blois")
-];
+// function qui cree un objet ville a partir d un id et d un nom
+function createVille(id, nom) {
+    link = "https://geocoding-api.open-meteo.com/v1/search?name=" + nom + "&count=1&language=fr&format=json";
+    data = getDataSync(link);
+    if (data.results) {
+        nom = data.results[0].name;
+        latitude = data.results[0].latitude;
+        longitude = data.results[0].longitude;
+        console.log("Ville créée : " + nom + " (id: " + id + ", latitude: " + latitude + ", longitude: " + longitude + ")");
+        return new Ville(id, nom, latitude, longitude);
+    } else {
+        alert("Ville non trouvée");
+        return null;
+    }
+}
+
+const villes = [];
+
+ville = createVille(1, "Blois");
+if (ville) {
+    villes.push(ville);
+}
 
 // let ville = new Ville(1, "Blois");
 
@@ -177,25 +204,27 @@ const villes = [
 // updateCard(ville, data);
 
 function addCard() {
-    let id = document.querySelectorAll('.card').length + 1;
+    let id = villes.length + 1;
     let nom = prompt("Entrez le nom de la ville :");
-    let ville = new Ville(id, nom);
+    let ville = createVille(id, nom);
+    if (ville) {
+        villes.push(ville);
+        newCard(ville.getId());
+        data = getDataSync("https://api.open-meteo.com/v1/forecast?latitude=" + ville.getLatitude() + "&longitude=" + ville.getLongitude() + "&daily=weather_code,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Europe%2FBerlin&forecast_days=1");
+        updateCard(ville, data);
+    }
+}
+
+function createCard(ville) {
     newCard(ville.getId());
+    data = getDataSync("https://api.open-meteo.com/v1/forecast?latitude=" + ville.getLatitude() + "&longitude=" + ville.getLongitude() + "&daily=weather_code,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Europe%2FBerlin&forecast_days=1");
     updateCard(ville, data);
 }
 
-function addCard(ville, data) {
-    newCard(ville.getId());
-    updateCard(ville, data);
-}
-
-addCard(villes[0], data);
-
+createCard(villes[0]);
 
 const addCardBtn = document.getElementById('add-card-btn');
-addCardBtn.addEventListener('click', addCard);   
-document.getElementById('add-card-btn').addEventListener('click', addCard); 
-
+addCardBtn.addEventListener('click', addCard);
 
 function refresh() {
     let cards = document.querySelectorAll('.card');
@@ -205,6 +234,83 @@ function refresh() {
         updateCard(ville, data);
     });
 }
+
+// fonction filtre qui demande a l utilisateur de choisir les info de meteo detailles qu il veut voir et qui affiche seulement ces info dans les cards avec un formulaire dans une pop up de checkbox pour chaque info (temp min, temp max, humidity, wind) et un bouton pour valider le choix de l utilisateur et appliquer le filtre sur les cards
+function filtre() {
+    let filtrePopup = document.createElement('div');
+    filtrePopup.className = "filtre-popup";
+    document.body.appendChild(filtrePopup);
+
+    let form = document.createElement('form');
+    filtrePopup.appendChild(form);
+    let tempMinCheckbox = document.createElement('input');
+    tempMinCheckbox.type = "checkbox";
+    tempMinCheckbox.id = "temp-min-checkbox";
+    form.appendChild(tempMinCheckbox);
+    let tempMinLabel = document.createElement('label');
+    tempMinLabel.htmlFor = "temp-min-checkbox";
+    tempMinLabel.innerHTML = "Température minimale";
+    form.appendChild(tempMinLabel);
+    let tempMaxCheckbox = document.createElement('input');
+    tempMaxCheckbox.type = "checkbox";
+    tempMaxCheckbox.id = "temp-max-checkbox";
+    form.appendChild(tempMaxCheckbox);
+    let tempMaxLabel = document.createElement('label');
+    tempMaxLabel.htmlFor = "temp-max-checkbox";
+    tempMaxLabel.innerHTML = "Température maximale";
+    form.appendChild(tempMaxLabel);
+    let humidityCheckbox = document.createElement('input');
+    humidityCheckbox.type = "checkbox";
+    humidityCheckbox.id = "humidity-checkbox";
+    form.appendChild(humidityCheckbox);
+    let humidityLabel = document.createElement('label');
+    humidityLabel.htmlFor = "humidity-checkbox";
+    humidityLabel.innerHTML = "Humidité";
+    form.appendChild(humidityLabel);
+    let windCheckbox = document.createElement('input');
+    windCheckbox.type = "checkbox";
+    windCheckbox.id = "wind-checkbox";
+    form.appendChild(windCheckbox);
+    let windLabel = document.createElement('label');
+    windLabel.htmlFor = "wind-checkbox";
+    windLabel.innerHTML = "Vent";
+    form.appendChild(windLabel);
+    let submitButton = document.createElement('button');
+    submitButton.type = "submit";
+    submitButton.innerHTML = "Valider";
+    form.appendChild(submitButton);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let tempMinChecked = tempMinCheckbox.checked;
+        let tempMaxChecked = tempMaxCheckbox.checked;
+        let humidityChecked = humidityCheckbox.checked;
+        let windChecked = windCheckbox.checked;
+        let cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            let tempMin = card.querySelector('.temp-min');
+            let tempMax = card.querySelector('.temp-max');
+            let humidity = card.querySelector('.humidity');
+            let wind = card.querySelector('.wind');
+            if (tempMin) {
+                tempMin.style.display = tempMinChecked ? "block" : "none";
+            }
+            if (tempMax) {
+                tempMax.style.display = tempMaxChecked ? "block" : "none";
+            }
+            if (humidity) {
+                humidity.style.display = humidityChecked ? "block" : "none";
+            }
+            if (wind) {
+                wind.style.display = windChecked ? "block" : "none";
+            }
+        });
+        document.body.removeChild(filtrePopup);
+    });
+}
+
+const filtreBtn = document.getElementById('filtre');
+filtreBtn.addEventListener('click', filtre);
 
 const villeFile = [];
 
@@ -233,8 +339,4 @@ function convertDate(dateStr) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return date.toLocaleDateString('fr-FR', options);
 }
-    
-
-
-
 
